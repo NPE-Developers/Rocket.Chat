1824f263f0758c4d117a61cf4b6e1d99379b62db
Manu@ares-xps15 <manuel.turetta94@gmail.com>
2017-06-16 16:09:29 +0200

    Fix e miglioramenti - Coretto il file .meteor/packages che conteneva i riferimenti a monolith e a bringit - Aggiunto uno script per eseguire il setup di monolith e bringit in automatico

---
+++ npe_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MONOLITH_URL="https://github.com/NPE-Developers/Monolith"
+BRINGIT_URL="https://github.com/NPE-Developers/BringIt"
+
+if [[ -z "$1" ]] && [[ $1 == '-d' ]]; then
+    echo "====== Development Mode ======"
+    $MONOLITH_URL="git@github.com:NPE-Developers/Monolith.git"
+    $BRINGIT_URL="git@github.com:NPE-Developers/BringIt.git"
+fi
+
+git clone $MONOLITH_URL ./packages/monolith
+git clone $BRINGIT_URL ./packages/bringit
+meteor add monolith
+meteor add bringit
